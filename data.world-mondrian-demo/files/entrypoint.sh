e303b2a651c38b6e0f0e867c6c6d0dbaad02b26e
Scott Came <scottcame10@gmail.com>
2018-08-30 21:21:31 -0700

    Remove perl dependency

--- data.world-mondrian-demo/files/entrypoint.sh
+++ data.world-mondrian-demo/files/entrypoint.sh
@@ -6,6 +6,9 @@ if [ "$DW_TOKEN" == "<placeholder>" ]; then
   echo 'Run docker with --env DW_TOKEN="<your data.world auth token>"'
   exit 1
 fi
-perl -pi -e 's/%DW_TOKEN%/$ENV{"DW_TOKEN"}/' /usr/local/tomcat/shared/mondrian-connections.json
+
+echo $DW_TOKEN
+
+sed -i "s/%DW_TOKEN%/$DW_TOKEN/" /usr/local/tomcat/shared/mondrian-connections.json
 
 catalina.sh run
